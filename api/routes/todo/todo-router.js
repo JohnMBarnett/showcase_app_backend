6cd8ca3d39315bfaeea5e66d0690bf3c568d8666
onuri5 <jomibaofficial@gmail.com>
2022-02-23 22:20:50 -0600

    can now create todos and delete todos

--- api/routes/todo/todo-router.js
+++ api/routes/todo/todo-router.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
+const { createToDo, deleteToDo } = require("./todo-model");
 
-router.get("/", (req, res, next) => {
-    
-})
+router.post("/", (req, res, next) => {
+  createToDo(req.body)
+    .then((ToDos) => {
+      res.status(200).json(ToDos);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.put("/", (req, res, next) => {
+  deleteToDo(req.body)
+    .then((ToDos) => {
+      res.status(201).json(ToDos);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
--- api/routes/user/user-model.js
+++ api/routes/user/user-model.js
@@ -9,18 +9,23 @@ const getUser = (filter) => {
 };
 
 const addUser = async (newUser) => {
-  const [newObj] = await db("users").insert(newUser, ["user_id", "username", "password", "user_email"]);
+  const [newObj] = await db("users").insert(newUser, [
+    "user_id",
+    "username",
+    "password",
+    "user_email",
+  ]);
   return newObj;
 };
 
 const getUserTasks = async (filter) => {
   const [user] = await db("users").where(filter);
-  return db("todos").where({owner_id: user.user_id});
-}
+  return db("todos").where({ owner_id: user.user_id });
+};
 
 module.exports = {
   getAllUsers,
   addUser,
   getUser,
-  getUserTasks
+  getUserTasks,
 };
