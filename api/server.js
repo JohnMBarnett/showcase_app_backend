43f6f6d3ca0926dfa41fed94f10739d5978c1a92
onuri5 <jomibaofficial@gmail.com>
2022-02-23 18:40:49 -0600

    set index file and basic server file

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const userRouter = require("./routes/user/user-router");
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.use("/api/users", userRouter);
+
+module.exports = server;
